22f0d8f92a40d4532fc7d6c2b487e0fbc03e53c7
Thomas Roosdorp <thomas.roosdorp@gmail.com>
2023-01-06 15:52:01 +0100

    Update libs.R

--- R/libs.R
+++ R/libs.R
@@ -1,14 +1,20 @@
 
 # Packages that are accessible to lib functions.
-packages <- list(base = list("tidyverse", "writexl", "remotes", "fs", "rlang",
-                         "pbapply", "textclean", "foreach", "doParallel"),
-             epi = list("Epi", "MortalityTables"),
-             stat = list("infer", "pwr", "naivebayes", "pROC", "rpart",
-                         "randomForest", "vtreat", "reclin", "sigr", "WVPlots"),
-             mdown = list("rmarkdown", "bookdown", "tinytex", "knitr"),
-             bench = list("lobstr", "profvis","benchmarkme", "bench"),
-             dev = list("devtools", "roxygen2", "styler",
-                        "lintr", "withr", "available", "R6")
+packages <- list(
+
+  base  = list("tidyverse", "remotes", "fs", "pbapply", "textclean",
+               "foreach", "doParallel"),
+  data  = list("DBI", "XLConnect", "writexl", "foreign", "haven",
+               "sweidnumbr", "httr"),
+  epi   = list("Epi", "MortalityTables"),
+  stat  = list("infer", "pwr", "naivebayes", "pROC", "reclin", "sigr",
+               "WVPlots", "xgboost"),
+  model = list("tidymodels", "mgcv", "randomForest", "vcd", "rpart",
+               "ranger", "vtreat", "survival"),
+  report= list("rmarkdown", "bookdown", "tinytex", "knitr", "shiny"),
+  bench = list("lobstr", "profvis","benchmarkme", "bench"),
+  dev   = list("devtools", "roxygen2", "styler", "lintr",
+               "withr", "available", "R6")
 )
 
 usethis::use_data(packages, internal = TRUE, overwrite = TRUE)
