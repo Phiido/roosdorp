bf022bf252c71a8f912601fdcfb397a997d47c81
Thomas Roosdorp <thomas.roosdorp@gmail.com>
2023-04-25 09:43:26 +0200

    Update libs.R
    Added more packages.

--- R/libs.R
+++ R/libs.R
@@ -3,18 +3,20 @@
 packages <- list(
 
   base  = list("tidyverse", "remotes", "fs", "pbapply", "textclean",
-               "foreach", "doParallel"),
+               "foreach", "doParallel", "reticulate"),
   data  = list("DBI", "XLConnect", "writexl", "foreign", "haven",
                "sweidnumbr", "httr"),
-  epi   = list("Epi", "MortalityTables"),
+  epi   = list("Epi", "MortalityTables", "Survival", "ggsurvfit"),
   stat  = list("infer", "pwr", "WebPower","naivebayes", "pROC", "reclin",
-               "sigr", "WVPlots", "xgboost", "random"),
+               "sigr", "WVPlots", "xgboost", "random", "rsimsum"),
   model = list("tidymodels", "mgcv", "randomForest", "vcd", "rpart",
                "ranger", "vtreat", "survival", "gbm", "caret"),
   report= list("rmarkdown", "bookdown", "tinytex", "knitr", "shiny"),
+  shiny = list("DT", "leaflet", "plotly", "shinythemes", "shinydashboard",
+               "shinyBS", "shinyWidgets", "formattable", "shinyjs"),
   bench = list("lobstr", "profvis","benchmarkme", "bench"),
   dev   = list("devtools", "roxygen2", "styler", "lintr",
-               "withr", "available", "R6")
+               "withr", "available", "R6", "shinytest", "shinyloadtest")
 )
 
 usethis::use_data(packages, internal = TRUE, overwrite = TRUE)
