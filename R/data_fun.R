8a2d23369ed7e21a99c189de13b2eeab6d62e15c
Phiido <github@roosdorp.se>
2024-06-01 15:20:09 +0200

    Fix label_vars

--- R/data_fun.R
+++ R/data_fun.R
@@ -5,31 +5,43 @@
 #'  to label variables used in plots or tables.
 #'
 #' @param data Input as data frame or character.vector
-#' @param dict Either data.frame or file.path to dictionary
+#' @param dict Input as data.frame with two columns. Should closely follow a key value structure
 #' @param simple Logical; Should return as character.vector? Otherwise renamed data.frame
 #'
-#' @return Data.frame or Character vector
+#' @return Data.frame or character.vector
 #'
 #' @export
 label_vars <- function(data,
                        dict = NULL,
                        simple = FALSE) {
 
-  base::stopifnot(base::is.data.frame(data), base::is.character(data))
+  base::stopifnot(base::is.data.frame(data) | base::is.character(data),
+                  base::is.data.frame(dict),
+                  base::is.logical(simple))
 
-  dictionary <- get('dictionary', envir = .ProjectEnv)
+  if (is.character(data) & length(data) !=2) stop('Dictionary must have length of 2')
 
-  group <- switch(dict,
-                  data      = dictionary$data,
-                  food      = dictionary$food,
-                  foodgroup = dictionary$foodgroup,
-                  model     = dictionary$model,
-                  vars      = dictionary$vars)
+  # TODO add functionality by reading rownames
 
-  group <- group[c('id','description')] |>
-    as.data.frame()
+  if (base::is.data.frame(data)) {
+    names_vars <- base::names(data)
+  } else {
+    names_vars <- data
+  }
 
-  names(data) <- group[match(names(data), group$id), 'description']
+  # for readability
+  names(dict) <- c('key', 'value')
 
-  invisible(data)
+  vars <- NULL
+
+  for (name in names_vars) {
+    if (name %in% dict[,'key']) {
+      vars <- append(vars, dict[dict[, 'key'] == name, 'value'])
+    } else vars <- append(vars, name)
+  }
+
+  if (is.data.frame(data)) names(data) <- vars
+
+  if (simple & is.data.frame(data)) return(names(data))
+  else return(data)
 }
