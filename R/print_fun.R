1770410c6f8cd9a2953ba1ad9ede86bc669631ca
Phiido <github@roosdorp.se>
2024-06-01 15:29:51 +0200

    Update print_rmd

--- R/print_fun.R
+++ R/print_fun.R
@@ -1,8 +1,7 @@
 #' Helper function for easier printing of Rmd files
 #'
-#' `print_rmd()`returns labels from a data dictionary, either renaming the columns or
-#'  returns a character vector with the labels. The latter may be useful when wanting
-#'  to label variables used in plots or tables.
+#' `print_rmd()` can be seen as an extension of rmarkdown::render(). You can specify parameters,
+#' output folder and whether to open it up after finishing the render.
 #'
 #' @param name Name of Rmd file that will be used. Locates in './Rmd/'.
 #' @param params Pass along parameters/data to the Rmd
@@ -18,6 +17,8 @@ print_rmd <- function(name, params = NULL, output_path = './output', open = FALS
   base::stopifnot(base::is.character(name), base::is.character(output_path))
 
   rmd_file <- base::paste0('./Rmd/', name, '.Rmd')
+
+  # TODO handle other file types in a more flexible way
   output_file <- base::file.path(output_path, type, '.pdf')
 
   base::stopifnot(base::file.exists(rmd_file))
